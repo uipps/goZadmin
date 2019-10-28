2820ec70b6f47bff83aa36df1df5fdab35a52141
tom-cf <red_boxer@sohu.com>
2019-10-28 16:27:25 +0800

    第一个测试go程序

--- test01.go
+++ test01.go
@@ -1,7 +1,18 @@
+// 测试主程序，有main包和main函数
 package main
 
 import "fmt"
 
+const s string = "abcde"
+
 func main() {
+	var str01 string = "haha"
+	var int01, int02 int = 2, 3
+	const C = 3e20
+	fmt.Println(str01)
+	fmt.Println(int64(int01), int02, C)
+	fmt.Println(s)
 	fmt.Println("go,test01")
+	fmt.Println("1+1=", 1+1)
+	fmt.Println(true)
 }
