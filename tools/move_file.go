9ae221cb24255e435e7d16ab29080e9d59ccba46
tom-cf <uipps_com@163.com>
2021-01-12 14:58:03 +0800

    文件名修改功能，执行另一个func

--- tools/move_file.go
+++ tools/move_file.go
@@ -1,6 +1,8 @@
 // 将某文件或某一类型的文件，移动到指定的目录下
 //		例如，将分散的mp4视频文件，转移到统一的目录下
 // 	go run move_file.go -p "E:/BaiduNetdiskDownload/aa/aa_bb" -t "E:/qianfeng_golang_mp4/" -T ".mp4"
+//  go run move_file.go -p "H:/qianfeng_golang_mp4" -t "H:/qianfeng_golang_mp4" -M "fileNameReplaceStr" -S "千锋Go语言教程：/千锋GO语言教程：" -R ""
+//  go run move_file.go -p "H:/qianfeng_golang_mp4" -t "H:/qianfeng_golang_mp4" -M "fileNameReplaceStr" -S "千锋Go语言教程："
 
 package main
 
@@ -18,12 +20,18 @@ var (
 	toPath     string // 目标路径
 	isSon      int    // 是否递归子目录
 	fileType   string // 需要移动的文件类型
+	methodFunc string // 执行方法
+	strOrig    string // 被替换的字符串，用“/”分隔
+	replaceStr string // 替换成的字符串，
 )
 
 func init() {
 	flag.StringVar(&sourcePath, "p", "", "Usage: ")
 	flag.StringVar(&toPath, "t", "", "Usage: ")
 	flag.StringVar(&fileType, "T", ".mp4", "Usage: 1 0")
+	flag.StringVar(&methodFunc, "M", "", "fileNameReplaceStr")
+	flag.StringVar(&strOrig, "S", "", "需要被替换的字符串，多个用“/”分隔")
+	flag.StringVar(&replaceStr, "R", "", "替换成的字符串，暂时仅仅支持一条")
 	flag.IntVar(&isSon, "i", 1, "Usage: 1 0")
 }
 
@@ -46,7 +54,12 @@ func main() {
 	if "" == sourcePath || !FileOrPathExists(sourcePath) {
 		fmt.Printf("source path %s is empty or not exist! \n", sourcePath)
 	} else {
-		transPath(sourcePath)
+		if ("fileNameReplaceStr" == methodFunc) {
+			fileNameReplaceStr(sourcePath)
+		} else {
+			transPath(sourcePath)
+		}
+
 	}
 
 	return
@@ -109,5 +122,45 @@ func FilePutContents(name string, content string) {
 	data := []byte(content)
 	if ioutil.WriteFile(name, data, 0775) == nil {
 		//fmt.Println("写入文件成功:", content)
+	}
+}
+
+func fileNameReplaceStr(source_path string) {
+	files, err := ioutil.ReadDir(source_path)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		for _, v := range files {
+			//fmt.Printf("--- %v --- \n --- %q --- \n --- %p --- \n", v, v, v)
+			//fmt.Println("\n\n\n\n")
+			if v.IsDir() { //	&& 1 == isSon
+				// fmt.Println(source_path + "/" + v.Name())
+				transPath(source_path + "/" + v.Name())
+			} else {
+				fmt.Println(source_path + "/" + v.Name())
+				FilePutContents("D:/temp/go_write_test.txt", source_path+"/"+v.Name()+"\r\n")
+
+				// 重命名文件，即剪切到新位置，必须相同的盘符，否则：The system cannot move the file to a different disk drive.
+				tmpSplit := strings.Split(strOrig, "/")
+				new_file := v.Name()
+				for _, str01 := range tmpSplit {
+					//fmt.Printf("%T, %s \n", str01,str01)
+					new_file = strings.ReplaceAll(new_file, str01, replaceStr)
+				}
+
+				fmt.Printf(" -- new file name is %s \n", new_file)
+				err := os.Rename(source_path+"/"+v.Name(), toPath+"/"+new_file)
+				if err != nil {
+					//如果重命名文件失败,则输出错误 file rename Error!
+					fmt.Println("file rename Error!")
+					//打印错误详细信息
+					fmt.Printf("%s", err)
+				} else {
+					//如果文件重命名成功,则输出 file rename OK!
+					fmt.Println("file rename OK!")
+				}
+
+			}
+		}
 	}
 }
