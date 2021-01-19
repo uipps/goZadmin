9919595db9e54f3ef442595efa2a5ec554d4bc74
tom-cf <uipps_com@163.com>
2021-01-19 17:06:04 +0800

    去掉注释

--- tools/suanfa/hanoi/hanoi1.go
+++ tools/suanfa/hanoi/hanoi1.go
@@ -1,6 +1,18 @@
 // 汉诺塔：
 //  把A塔上编号从小号到大号的圆盘从A塔通过B辅助塔移动到C塔上去，要求大盘不能在小盘上面
 // go run F:\develope\go\go_code_path\src\github.com\uipps\goZadmin\tools\suanfa\hanoi\hanoi1.go -n 3
+// 不难发现规律：1个圆盘的次数 2的1次方减1
+//
+//　　　　　　　2个圆盘的次数 2的2次方减1
+//
+//                         3个圆盘的次数 2的3次方减1
+//
+//                         。  。   。    。   。
+//
+//                         n个圆盘的次数 2的n次方减1
+//
+// 故：移动次数为：2^n - 1
+//     调用方法的栈机制:（特点：先进后出）
 
 package main
 
@@ -25,14 +37,14 @@ func main() {
     b := "B"
     c := "C"
 
-    fmt.Println("******************************************************************************************");
-    fmt.Println("这是汉诺塔问题（把A塔上编号从小号到大号的圆盘从A塔通过B辅助塔移动到C塔上去）");
-    fmt.Println("    要求移动过程中，大盘不能放在小盘上面；从上到下编号依次为： 1 2 3 ...... n-1 n");
-    fmt.Println("******************************************************************************************");
-    fmt.Printf("圆盘的个数为：%d\n\n", hanoiNum);
+    fmt.Println("******************************************************************************************")
+    fmt.Println("这是汉诺塔问题（把A塔上编号从小号到大号的圆盘从A塔通过B辅助塔移动到C塔上去）")
+    fmt.Println("    要求移动过程中，大盘不能放在小盘上面；从上到下编号依次为： 1 2 3 ...... n-1 n")
+    fmt.Println("******************************************************************************************")
+    fmt.Printf("圆盘的个数为：%d\n\n", hanoiNum)
     hanoiCount = 0
     TowersOfHanoi1(hanoiNum, a, b, c)
-    fmt.Printf("\n>>移动了 %d 次，把A上的圆盘都移动到了C上", hanoiCount);
+    fmt.Printf("\n>>移动了 %d 次，把A上的圆盘都移动到了C上", hanoiCount)
 
     return
 }
@@ -53,7 +65,7 @@ func TowersOfHanoi1(num int, a string, b string, c string) {
     return
 }
 
-func printMove(num int, a string, c string)  {
+func printMove(num int, a string, c string) {
     hanoiCount++
-    fmt.Printf("第%4d 次移动 : 把 %3d 号圆盘从 %s ---> %s  \n", hanoiCount, num, a, c);
+    fmt.Printf("第%4d 次移动 : 把 %3d 号圆盘从 %s ---> %s  \n", hanoiCount, num, a, c)
 }
