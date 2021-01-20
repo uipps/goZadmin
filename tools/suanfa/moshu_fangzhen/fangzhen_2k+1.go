c3a1459a874fdec8fef1c710110b890f1264a76b
tom-cf <uipps_com@163.com>
2021-01-20 19:01:10 +0800

    奇数2k+1方阵填数游戏

---
+++ tools/suanfa/moshu_fangzhen/fangzhen_2k+1.go
@@ -0,0 +1,98 @@
+// 奇数魔术方阵(2k+1)：方阵中的每行、每列或对角线位置的数各自相加的和均相等，
+//  简捷连续填数法：1立首行中，右1上1，受阻下1，具体看下面解释
+//  (1) 将1放在第一行中间一列;
+//  (2) 从2开始直到n×n止各数依次按下列规则存放：每一个数存放的行比前一个数的列数加1，行数减1，
+//  (3) 如果行列范围超出矩阵范围，则回绕。例如1在第1行，则2应放在最下一行，列数同样加1;
+//  (4) 如果按上面规则确定的位置上已有数，或上一个数是第1行第n列时，则把下一个数放在上一个数的下面。
+
+// go run "F:\develope\go\go_code_path\src\github.com\uipps\goZadmin\tools\suanfa\moshu_fangzhen\fangzhen_2k+1.go" -b 1 -n 3
+// go run "F:\develope\go\go_code_path\src\github.com\uipps\goZadmin\tools\suanfa\moshu_fangzhen\fangzhen_2k+1.go" -n 5
+/**
+
+请输入矩阵行数(奇数):3
+ 8    1    6
+ 3    5    7
+ 4    9    2
+
+*/
+
+package main
+
+import (
+    "flag"
+    "fmt"
+    "time"
+)
+
+var (
+    argN1     int
+    argBase1  int   // 从几开始的基数，默认是从1开始
+    arrYiWei1 []int // 用1维数组存放数字好了，TODO 以后改成二维数组
+)
+
+func init() {
+    flag.IntVar(&argN1, "n", 3, "Usage: 5 6 7")
+    flag.IntVar(&argBase1, "b", 1, "Usage: 1")
+}
+
+func main() {
+    flag.Parse()
+
+    startTime := time.Now().UnixNano()
+    fmt.Printf("startTime：%d, %s\n", startTime/1e3, time.Unix(0, startTime).Format("2006-01-02 15:04:05"))
+
+    // 2k+1矩阵
+    Magic1(argN1)
+
+    // 执行时间计算
+    endTime := time.Now().UnixNano()
+    fmt.Printf("  endTime：%d, %s\n", endTime/1e3, time.Unix(0, endTime).Format("2006-01-02 15:04:05"))
+    nanoSeconds := float64(endTime-startTime) / 1e3
+    fmt.Println("spendTime：", nanoSeconds)
+}
+
+
+// 生成方阵的函数
+func Magic1(n int) int {
+    if (n%2 == 0 || n < 3) {
+        return 0 // 偶数返回
+    }
+
+    matrix := [4][4] int {} // {0,0,0}, {0,0,0}, {0,0,0}
+
+    i := 1 // 行号，从1开始吧
+    k := 1
+    j := n / 2 + 1 // 行中间
+
+    matrix[i][j] = k
+
+    for k = 2; k <= n*n; k++ {
+        tmpi := i;
+        tmpj := j;
+        if (j == n) {
+            j = 1;
+        } else {
+            j++;
+        }
+        if (i == 1) {
+            i = n;
+        } else {
+            i--;
+        }
+        if (matrix[i][j] != 0) {
+            i = tmpi + 1;
+            j = tmpj;
+        }
+        matrix[i][j] = k;
+    }
+
+    // 输出
+    for i := 1; i <= n; i++ {
+        for j := 1; j <= n; j++ {
+            fmt.Printf("%2d   ", matrix[i][j]);
+        }
+        fmt.Println();
+    }
+
+    return 1
+}
