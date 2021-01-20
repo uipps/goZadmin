cabeeec4be4cc2f48e20c7434639b3a2716acc7c
tom-cf <uipps_com@163.com>
2021-01-20 11:19:09 +0800

    习惯问题，下标从1开始，而不是0；arrYiWei[0] 不用并扩容数组增加了一位。a[1] a[2] a[3]

--- tools/suanfa/moshu_fangzhen/fangzhen.go
+++ tools/suanfa/moshu_fangzhen/fangzhen.go
@@ -35,74 +35,77 @@ func main() {
 }
 
 /**
-   a[0]    a[1]    a[2]
-   a[3]    a[4]    a[5]
-   a[6]    a[7]    a[8]
-a[0]的取值，从1~9遍历 (1+base ~ 9+base), 其他则从剩下的数据进行便利
+   a[1]    a[2]    a[3]
+   a[4]    a[5]    a[6]
+   a[7]    a[8]    a[9]
+a[1]的取值，从1~9遍历 (base+0 ~ 8+base), 其他则从剩下的数据进行便利
 
 */
 func fangzhen3jie(n int) {
-    // 初始化数组
-    var a,b,c,d,e,f,g,h int
+    //var a,b,c,d,e,f,g,h int
 
+    // 初始化数组
     // 三阶，总共9个数，逐个试探
     total := n * n
-    for i := 0; i < total; i++ {
-        arrYiWei = append(arrYiWei, argBase+i) // 下标从0开始
+    for i := 0; i <= total; i++ {
+        arrYiWei = append(arrYiWei, argBase + i - 1) // 下标从0开始，但是0下标不用，从1开始，使得a[1]=base; 并让数组长度增加1位
     }
     fmt.Println(arrYiWei)
 
-    for a = 0; a < total; a++ {
-        arrYiWei[0] = argBase + a
-        for b = 0; b < total; b++ {
+    // arrYiWei[0]，由于习惯问题， 下标0不用，从1开始编号
+
+    for a := 0; a < total; a++ {
+        arrYiWei[1] = argBase + a
+        for b := 0; b < total; b++ {
             if (a == b) {
                 // 数字不能相同
                 continue
             }
-            arrYiWei[1] = argBase + b
-            for c = 0; c < total; c++ {
+            arrYiWei[2] = argBase + b
+            for c := 0; c < total; c++ {
                 if (c == a || c == b) {
                     continue
                 }
-                arrYiWei[2] = argBase + c
-                for d = 0; d < total; d++ {
+                arrYiWei[3] = argBase + c
+                for d := 0; d < total; d++ {
                     if (d == a || d == b || d == c) {
                         continue
                     }
-                    arrYiWei[3] = argBase + d
-                    for e = 0; e < total; e++ {
+                    arrYiWei[4] = argBase + d
+                    for e := 0; e < total; e++ {
                         if (e == a || e == b || e == c || e == d) {
                             continue
                         }
-                        arrYiWei[4] = argBase + e
-                        for f = 0; f < total; f++ {
+                        arrYiWei[5] = argBase + e
+                        for f := 0; f < total; f++ {
                             if (f == a || f == b || f == c || f == d || f == e) {
                                 continue
                             }
-                            arrYiWei[5] = argBase + f
-                            for g = 0; g < total; g++ {
+                            arrYiWei[6] = argBase + f
+                            for g := 0; g < total; g++ {
                                 if (g == a || g == b || g == c || g == d || g == e || g == f) {
                                     continue
                                 }
-                                arrYiWei[6] = argBase + g
-                                for h = 0; h < total; h++ {
+                                arrYiWei[7] = argBase + g
+                                for h := 0; h < total; h++ {
                                     if (h == a || h == b || h == c || h == d || h == e || h == f || h == g) {
                                         continue
                                     }
-                                    arrYiWei[7] = argBase + h
+                                    arrYiWei[8] = argBase + h
                                     for i := 0; i < total; i++ {
                                         if (i == a || i == b || i == c || i == d || i == e || i == f || i == g || i == h) {
                                             continue
                                         }
-                                        arrYiWei[8] = argBase + i
+                                        arrYiWei[9] = argBase + i
                                         // 每行、每列、对角线分别都相等，则打印出此时的数据
-                                        if (arrYiWei[0]+arrYiWei[1]+arrYiWei[2] == arrYiWei[3]+arrYiWei[4]+arrYiWei[5] &&
-                                            arrYiWei[0]+arrYiWei[1]+arrYiWei[2] == arrYiWei[6]+arrYiWei[7]+arrYiWei[8] &&
-                                            arrYiWei[0]+arrYiWei[1]+arrYiWei[2] == arrYiWei[0]+arrYiWei[3]+arrYiWei[6] &&
-                                            arrYiWei[0]+arrYiWei[1]+arrYiWei[2] == arrYiWei[1]+arrYiWei[4]+arrYiWei[7] &&
-                                            arrYiWei[0]+arrYiWei[1]+arrYiWei[2] == arrYiWei[2]+arrYiWei[5]+arrYiWei[8] &&
-                                            arrYiWei[0]+arrYiWei[1]+arrYiWei[2] == arrYiWei[0]+arrYiWei[4]+arrYiWei[8] &&
-                                            arrYiWei[0]+arrYiWei[1]+arrYiWei[2] == arrYiWei[2]+arrYiWei[4]+arrYiWei[6]) {
+                                        t_he := arrYiWei[1]+arrYiWei[2]+arrYiWei[3]
+                                        if (t_he == arrYiWei[4]+arrYiWei[5]+arrYiWei[6] &&
+                                            t_he == arrYiWei[7]+arrYiWei[8]+arrYiWei[9] &&
+                                            t_he == arrYiWei[1]+arrYiWei[4]+arrYiWei[7] &&
+                                            t_he == arrYiWei[2]+arrYiWei[5]+arrYiWei[8] &&
+                                            t_he == arrYiWei[3]+arrYiWei[6]+arrYiWei[9] &&
+                                            t_he == arrYiWei[1]+arrYiWei[5]+arrYiWei[9] &&
+                                            t_he == arrYiWei[3]+arrYiWei[5]+arrYiWei[7]) {
                                             print_juzheng(arrYiWei, n)
                                             fmt.Println("\n")   // 增加2个换行
                                         }
@@ -121,10 +124,9 @@ func fangzhen3jie(n int) {
 
 // 格式化输出矩阵，通过一维数组输出
 func print_juzheng(arr []int, n int) {
-    //var length,i int
     length := len(arr)
-    for i := 0; i < length; i++ {
-        if 0 == i%3 {
+    for i := 1; i < length; i++ {
+        if 0 == (i-1)%3 {
             fmt.Println("") // 换行
         }
         fmt.Printf("%3d", arr[i])
