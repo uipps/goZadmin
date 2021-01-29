bdd2d9ef4220540b85ddc5c05efebf36732fdfaf
tom-cf <uipps_com@163.com>
2021-01-29 10:45:18 +0800

    对深层嵌套进行美化

--- suanfa/tianshu/tian_fuhao.go
+++ suanfa/tianshu/tian_fuhao.go
@@ -89,48 +89,52 @@ func tianfuhao01(arg_5Num string, arg_NumResult float64) {
     oper := [5]string{" ", "+", "-", "*", "/"}; // 运算符, 1表示+ ，2 表示-,3 表示* ，4 表示/
 
     for i[1] = 1; i[1] <= 4; i[1]++ { // 循环4种运算符，1表示+ ，2 表示-,3 表示* ，4 表示/
-        if ((i[1] < 4) || (num[2] != 0)) { // 运算符若是/, 则第二个运算数不能为0
-            for i[2] = 1; i[2] <= 4; i[2]++ {
-                if ((i[2] < 4) || (num[3] != 0)) {
-                    for i[3] = 1; i[3] <= 4; i[3]++ {
-                        if ((i[3] < 4) || num[4] != 0) {
-                            for i[4] = 1; i[4] <= 4; i[4]++ {
-                                if ((i[4] < 4) || (num[5] != 0)) {
-                                    left = 0;
-                                    right = num[1];
-                                    sign = 1;
-                                    for j := 1; j <= 4; j++ {
-                                        switch oper[i[j]] {
-                                        case "+":
-                                            left = left + float64(sign)*right;
-                                            sign = 1;
-                                            right = num[j+1];
-                                            break;
-                                        case "-":
-                                            left = left + float64(sign)*right;
-                                            sign = -1;
-                                            right = num[j+1];
-                                            break; // 通过f=-1实现减法
-                                        case "*":
-                                            right = right * num[j+1];
-                                            break; // 实现乘法
-                                        case "/":
-                                            right = right / num[j+1]; // 实现除法
-                                            break;
-                                        }
-                                    }
-                                    if (left+float64(sign)*right == result) {
-                                        count++;
-                                        fmt.Printf("%3d：", count);
-                                        for j := 1; j <= 4; j++ {
-                                            fmt.Printf("%.f%s", num[j], oper[i[j]]);
-                                        }
-
-                                        fmt.Printf("%.f=%.f\n", num[5], result);
-                                    }
-                                }
-                            }
+        if ((i[1] == 4) && (num[2] == 0)) { // 运算符若是/, 则第二个运算数不能为0
+            continue
+        }
+        for i[2] = 1; i[2] <= 4; i[2]++ {
+            if ((i[2] == 4) && (num[3] == 0)) {
+                continue
+            }
+            for i[3] = 1; i[3] <= 4; i[3]++ {
+                if ((i[3] == 4) && num[4] == 0) {
+                    continue
+                }
+                for i[4] = 1; i[4] <= 4; i[4]++ {
+                    if ((i[4] == 4) && (num[5] == 0)) {
+                        continue
+                    }
+                    left = 0;
+                    right = num[1];
+                    sign = 1;
+                    for j := 1; j <= 4; j++ {
+                        switch oper[i[j]] {
+                        case "+":
+                            left = left + float64(sign)*right;
+                            sign = 1;
+                            right = num[j+1];
+                            break;
+                        case "-":
+                            left = left + float64(sign)*right;
+                            sign = -1;
+                            right = num[j+1];
+                            break; // 通过f=-1实现减法
+                        case "*":
+                            right = right * num[j+1];
+                            break; // 实现乘法
+                        case "/":
+                            right = right / num[j+1]; // 实现除法
+                            break;
                         }
+                    }
+                    if (left+float64(sign)*right == result) {
+                        count++;
+                        fmt.Printf("%3d：", count);
+                        for j := 1; j <= 4; j++ {
+                            fmt.Printf("%.f%s", num[j], oper[i[j]]);
+                        }
+
+                        fmt.Printf("%.f=%.f\n", num[5], result);
                     }
                 }
             }
