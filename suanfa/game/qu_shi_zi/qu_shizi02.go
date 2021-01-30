8dceb06b1893a604b17de8d049043277f5e6bbb6
tom-cf <uipps_com@163.com>
2021-01-30 18:19:22 +0800

    优化用户体验

--- suanfa/game/qu_shi_zi/qu_shizi02.go
+++ suanfa/game/qu_shi_zi/qu_shizi02.go
@@ -18,8 +18,8 @@
 */
 
 //     go run qu_shizi.go -n 21
-//     go run /Users/cf/develope/go/go_code/src/github.com/uipps/goZadmin/suanfa/game/qu_shi_zi/qu_shizi.go -n 21
-//     go run F:\develope\go\go_code_path\src\github.com\uipps\goZadmin\suanfa\game\qu_shi_zi\qu_shizi.go -n 22
+//     go run /Users/cf/develope/go/go_code/src/github.com/uipps/goZadmin/suanfa/game/qu_shi_zi/qu_shizi02.go -n 21
+//     go run F:\develope\go\go_code_path\src\github.com\uipps\goZadmin\suanfa\game\qu_shi_zi\qu_shizi02.go -n 22
 
 package main
 
@@ -40,39 +40,94 @@ func init() {
 func main() {
     flag.Parse()
 
-    guize02()
+    fmt.Printf(" ------ 游戏规则是： 两人轮流取，每人每次可以取走1至4根，不可多取，也不能不取，谁取最后一根火柴谁输。\n\n")
+    guize02(argTotal02)
+
+    // 命令行输入参数
+    /*var huochai_num int
+    fmt.Printf("  请输入火柴数量(默认21)：")
+    _, err :=fmt.Scan(&huochai_num)
+    if err != nil {
+        fmt.Printf( " %s == %s " , os.Stderr, err)
+        return
+    }
+    fmt.Printf("  s:%s , v:%v, d:%d,c:%c,q:%q, p:%p", huochai_num,huochai_num,huochai_num, huochai_num,huochai_num,huochai_num)
+    if (huochai_num < 1 || huochai_num > 40) {
+        fmt.Printf("\nyour choice is %d! too big or too small，use default 21\n", huochai_num)
+        huochai_num = argTotal02
+    }
+    guize02(huochai_num)
+    */
 }
 
 // 规则2：找到规律，原来是4+1一个循环组。 参考： https://blog.csdn.net/csy981848153/article/details/9005248
-func guize02() {
-    sum := 21;       //tick num, 5k+1，后手（电脑）赢；否则 TODO
-    //input = 0;       //for input
-    bBreak := false; //if user input the wrong num is true
+func guize02(sum int) {
+    //sum := 21       //tick num, 5k+1，则后手（电脑）赢；否则看对手是否失误
+    computer_win := true //电脑获胜
 
-    fmt.Printf(" 游戏规则是： 两人轮流取，每人每次可以取走1至4根，不可多取，也不能不取，谁取最后一根火柴谁输。\n\n")
+    fmt.Printf(" 火柴数量是：%d \n\n", sum)
 
+    countN := 0
     //game run
-    for sum > 4 {
-        fmt.Printf("now the number of ticks is: %d\n", sum)
+    for sum > 0  {
+        countN++
+
+        fmt.Printf("\n第%3d轮, now the number of ticks is: %d\n", countN, sum)
+
+        // 剩余1个提前退出
+        if sum == 1 {
+            computer_win = true
+            fmt.Printf("剩余1根，您不得不拿!\n")
+            break
+        }
 
         // 接收用户输入
-        fmt.Println("turn to your choice(range:1~4): ")
-        fmt.Scan(&input)
-
-        if (input < 1 || input > 4) {
-            fmt.Printf("your choice is out of the range(1~4)!\n")
-            bBreak = true;
-            break;
+        fmt.Printf("第%3d轮, turn to your choice(range:1~4): ", countN)
+        for {
+            fmt.Scan(&input)
+            if (input < 1 || input > 4 || input > sum ) {   // 用户拿走的不能大于sum数量
+                fmt.Printf("your choice is out of the range(1~4)，请重新输入!\n")
+            } else {
+                break
+            }
         }
-        fmt.Printf("computer choices the num is: %d\n", 5-input)    // 电脑总是拿走5-input数量
-        sum -= 5;
+        sum -= input
+
+        // 没有剩余，则电脑赢
+        if (0 == sum) {
+            fmt.Printf("您拿走了全部，抱歉，您输了!\n")
+            computer_win = true
+            break
+        } else if (1 == sum) {
+            fmt.Printf("剩余1根，电脑不得不拿!\n")
+            computer_win = false
+            break
+        }
+
+        // 电脑获取多少张？电脑为了赢，所以让剩下的数量为5k+1
+        computer_nazou := 1
+        yushu := sum % 5
+        if (1 == yushu) {
+            // 此时对方会赢，因此就拿走1个或者随机，等待对方失误
+            computer_nazou = 1
+        } else {
+            computer_nazou = yushu - 1
+            if (computer_nazou < 0) {   // yushu = 0整除的情况
+                computer_nazou += 5
+            }
+        }
+
+        fmt.Printf("第%3d轮, computer choices the num is: %d\n", countN, computer_nazou)    // 电脑总是拿走5-input数量
+        sum -= computer_nazou
     }
-    if bBreak {
-        fmt.Printf("because you input the wrong num,", input)
+
+    fmt.Println("\n")
+    // 判断谁输谁赢
+    if computer_win {
+        fmt.Printf(" computer is the winner!")
     } else {
-        fmt.Printf("the num of tick is %d, less than 4,", sum)
+        fmt.Printf(" you are the winner! computer is loser")
     }
-    fmt.Printf(" computer is the winner!")
 
     return
 }
