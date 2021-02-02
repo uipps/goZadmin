4ee49b615b185570156c7072c652486b1c790330
macbook_air-cf <red_boxer@sohu.com>
2021-02-02 23:04:31 +0800

    循环小数标记循环节，并给出循环节长度

--- suanfa/pai/chufa.go
+++ suanfa/pai/chufa.go
@@ -1,5 +1,10 @@
-// 计算除法，1/m小数点后任意位数（m>=2的正整数）
+// 计算除法，n/m小数点后任意位数（m>n>0的正整数）
 //
+//        如果是循环小数，打印第一个循环节，用括号
+//         1/7 = 0.(142857)
+//         1/19 = 0.(052631578947368421)
+//         1/95 = 0.(0105263157894736842)
+
 // go run chufa.go -n 1 -m 7 -l 100
 // go run F:\develope\go\go_code_path\src\github.com\uipps\goZadmin\tools\suanfa\pai\chufa.go -n 1 -m 7 -l 100
 // go run F:\develope\go\go_code_path\src\github.com\uipps\goZadmin\tools\suanfa\pai\chufa.go -n 201 -m 101 -l 1111
@@ -32,7 +37,7 @@ func main() {
     startTime := time.Now().UnixNano()
     fmt.Printf("startTime：%d, %s\n", startTime/1e3, time.Unix(0, startTime).Format("2006-01-02 15:04:05"))
 
-    if (fenMu1 <= 1) {
+    if fenMu1 <= 1 {
         fmt.Println("m分母数据有误，必须是大于1的正整数")
         return
     }
@@ -40,6 +45,9 @@ func main() {
     fmt.Printf("\n  计算 %d/%d 的值， 显示小数点后%d位\n\n", fenZi1, fenMu1, xiaoshuLeng1)
     chuFa01(fenZi1, fenMu1, xiaoshuLeng1)
     fmt.Println("\n")
+
+    fmt.Printf("\n  计算 %d/%d 的值， 记录循环位置\n", fenZi1, fenMu1)
+    chuFa02(fenZi1, fenMu1)
 
     // 执行时间计算
     endTime := time.Now().UnixNano()
@@ -68,4 +76,89 @@ func chuFa01(fenzi_orig int, fenmu int, xiaoshuLeng int) {
     common.OutPrintFmt(k_arr1, xiaoshuLeng, 1, fenzi_orig, fenmu)
 
     return
+}
+
+// 两个正整数相除，如果是循环小数，则记录循环小数的循环节起止位置；使用两个数组进行记录
+func chuFa02(fenzi_orig int, fenmu int) {
+    fenzi := fenzi_orig
+    isXunhuan := false
+    iBegin := 0 // 循环节点开始位置
+    iEnd := 0   // 循环节点开始位置
+    iYouxian := 0
+    arr_length := fenmu + 1 // 余数结果数组的长度，
+    //arr_length := 100
+
+    // 用数组存放结果
+    shang_arr := make([]int, arr_length) // 保存结果，因为不知道循环节的长度，所以后面需要用append
+    yushu_arr := make([]int, arr_length) // 保存余数，可能浪费空间!
+
+    for i := 0; fenzi != 0; i++ {
+        if i > fenmu {
+            shang_arr = append(shang_arr, fenzi/fenmu) // 长度继续增加
+        } else {
+            shang_arr[i] = fenzi / fenmu
+        }
+
+        yushu := fenzi % fenmu // 余数
+
+        // 检查余数是否存在，存在表示一轮循环结束
+        if 0 == yushu {
+            // 余数为0，说明有限小数
+            iYouxian = i
+            break
+        }
+        if (i == 0 && shang_arr[0] == 0) || i > 0 { // i==1之后就是小数部分
+            key, in_or_not := common.Int_in_array(yushu_arr, yushu)
+            if in_or_not {
+                isXunhuan = true
+                iBegin = key
+                iEnd = i
+            } else {
+                isXunhuan = false
+            }
+        }
+
+        // 注入余数数组
+        if i > fenmu {
+            yushu_arr = append(yushu_arr, yushu) // 数组长度继续增加
+        } else {
+            yushu_arr[i] = yushu
+        }
+
+        if isXunhuan {
+            // 循环小数，则提前退出
+            break
+        }
+
+        fenzi = yushu * 10 // 作为下个循环的分子
+    }
+    //fmt.Println(yushu_arr)
+
+    // 非循环小数
+    if !isXunhuan {
+        fmt.Printf(" %d/%d=", fenzi_orig, fenmu)
+        for i := 0; i <= iYouxian; i++ {
+            if i == 1 {
+                fmt.Printf(".") // 小数部分是从1以后开始都放到.后面
+            }
+            fmt.Printf("%d", shang_arr[i])
+        }
+    } else {
+        // 先输出非循环节部分；循环部分使用()括起来
+        fmt.Printf("无限循环小数: %d/%d=%d.", fenzi_orig, fenmu, shang_arr[0])
+
+        for i := 1; i <= iEnd; i++ {
+            if iBegin == 0 && i == 1 {
+                fmt.Printf("(") // 输出时循环节用 '()' 括起
+            }
+            fmt.Printf("%d", shang_arr[i])
+            if iBegin == i && i >= 1 {
+                fmt.Printf("(") // 输出时循环节用 '()' 括起
+            }
+        }
+        fmt.Printf(")") // 输出时循环节用 '()' 括起
+
+        fmt.Printf("从小数点后第%d位开始循环, 第%d位结束，循环节长度%d", iBegin+1, iEnd, iEnd-iBegin)
+    }
+    fmt.Printf("\n")
 }
