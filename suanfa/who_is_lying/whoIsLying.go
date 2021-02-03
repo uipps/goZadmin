3ce0f4fe528fc3d0abadcc37dc3e920f37de3b84
tom-cf <uipps_com@163.com>
2021-02-03 10:49:58 +0800

    增加新娘和新郎

--- suanfa/who_is_lying/whoIsLying.go
+++ suanfa/who_is_lying/whoIsLying.go
@@ -6,6 +6,8 @@
 参考： https://blog.csdn.net/csy981848153/article/details/7626784
 
 
+go run F:\develope\go\go_code_path\src\github.com\uipps\goZadmin\suanfa\who_is_lying\whoIsLying.go
+
 解：设张三为A，李四为B，王五为C，说真话为1，说假话为0．
 
 　　(1)若A＝1，即张三说真话．由于张三说：“李四在说谎”，可推知B＝0．
@@ -22,7 +24,7 @@
 
 　　李四说：“王五在说谎”，知C＝0．
 
-　　由于王五说：“张三和李四都说谎”，而C＝0，可得A＝1，B＝1或A＝0，B＝1，
+　　由于王五说：“张三和李四都说谎”，而C＝0，可得A＝1，B＝1或A＝0，B＝1，或A＝1，B＝0
 
 　　则C＝0符合要求．
 
@@ -43,27 +45,17 @@ func main() {
 
 func whoIsLying() {
     tem := 0
+    lying := [2]string{"说谎", "没说谎"} // 0-说假话、说谎；1-真话、没说谎
 
     for a := 1; a >= 0; a-- {
         for b := 1; b >= 0; b-- {
             for c := 1; c >= 0; c-- {
                 // 张三a说李四b在说谎; 李四b说王五c在说谎; 王五说张三和李四都在说谎
-                if (a+b == 1) && (b+c == 1) && (a+b+c == 1) {
-                    if 1 == a {
-                        fmt.Println("张三没说谎")
-                    } else {
-                        fmt.Println("张三说谎")
-                    }
-                    if 1 == b {
-                        fmt.Println("李四没说谎")
-                    } else {
-                        fmt.Println("李四说谎")
-                    }
-                    if 1 == c {
-                        fmt.Println("王五没说谎")
-                    } else {
-                        fmt.Println("王五说谎")
-                    }
+                // ( (a==1 && b+c == 0)||(a ==0 && b+c>=1) )  // (a==1 && b+c == 0) 不成立
+                if (a+b == 1) && (b+c == 1) && (a == 0 && b+c >= 1) {
+                    fmt.Printf("张三%s\n", lying[a])
+                    fmt.Printf("李四%s\n", lying[b])
+                    fmt.Printf("王五%s\n", lying[c])
                     tem = 1
                 }
             }
---
+++ suanfa/who_is_lying/xinniang_xinliang.go
@@ -0,0 +1,44 @@
+/*
+48. 新娘和新郎
+题目：三对情侣参加婚礼，三个新郞为A、B、C，三个新娘为X、Y、Z。有人不知道谁和谁结婚，于是询问了六位新人中的三位，
+但听到的回答是这样的：A说他将和X结婚；X说她的未婚夫是C；C说他将和Z结婚。这人听后知道他们在开玩笑，全是假话。请编程找出谁将和谁结婚。
+
+参考： https://blog.csdn.net/csy981848153/article/details/7626779
+
+go run F:\develope\go\go_code_path\src\github.com\uipps\goZadmin\suanfa\who_is_lying\xinniang_xinliang.go
+
+--
+新娘X和新郎B是一对情侣！
+新娘Y和新郎C是一对情侣！
+新娘Z和新郎A是一对情侣！
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+    xinniangXinlang()
+}
+
+func xinniangXinlang() {
+    //A,B,C := 1,2,3  // 用1，2，3表示三个不同的新郎，
+    //xinlang := map[int]string {1:"A", 2:"B", 3:"C"}   // 也可以用map
+    //xinlang := [...]string {" ", "A", "B", "C"} // 也可以用数组
+    xinlang := []string{" ", "A", "B", "C"} // 也可以用切片
+
+    x, y, z := 0, 0, 0 // 新娘设定初值，找出xyz哪个跟1，2，3相等就是对应的新娘
+
+    for x = 1; x <= 3; x++ {
+        for y = 1; y <= 3; y++ {
+            for z = 1; z <= 3; z++ {
+                if x != 1 && x != 3 && z != 3 && x != y && y != z && x != z {
+                    fmt.Printf("新娘x和新郎%s是一对情侣！\n"+
+                        "新娘y和新郎%s是一对情侣！\n"+
+                        "新娘z和新郎%s是一对情侣！\n", xinlang[x], xinlang[y], xinlang[z])
+                }
+            }
+        }
+    }
+}
